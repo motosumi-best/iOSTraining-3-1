13b174b38e194e1fdb7e7a193f923d8efcbb0f34
skaneko <skaneko@4cast.co.jp>
2014-06-30 12:56:53 +0900

    iOSTraining 3-1 新規作成

--- iOSTraning3-1/ViewController.m
+++ iOSTraning3-1/ViewController.m
@@ -17,7 +17,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:scrollView];
+    
 	// Do any additional setup after loading the view, typically from a nib.
+    UIImage *image = [UIImage imageNamed:@"1.JPG"];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, image.size.width, image.size.height)];
+    imageView.image = image;
+    
+    [scrollView addSubview:imageView];
+    
+    scrollView.contentSize = imageView.frame.size;
+    scrollView.maximumZoomScale = 3.0;
+    scrollView.minimumZoomScale = 0.5;
+    
+    scrollView.delegate = self;
+    
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    for (UIView *view in scrollView.subviews) {
+        if ([view isKindOfClass:[UIImageView class]]) {
+            return view;
+        }
+    }
+    return nil;
 }
 
 - (void)didReceiveMemoryWarning
